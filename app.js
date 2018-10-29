8438e254f6adef32ee229a89b556671aa4eb1750
heraldo <telUricosUcallao9>
2018-10-28 20:14:30 -0500

    update heroku deploy

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ var connect = require('connect');
 var serveStatic = require('serve-static');
 var port = process.env.PORT || 8080;
 
-connect().use(serveStatic(__dirname+ '/wwww')).listen(port, function(){
+connect().use(serveStatic(__dirname+ '/wwww/')).listen(port, function(){
+    console.log(__dirname+ '/wwww/');
     console.log('Server running on '+ port +'...');
 });
--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const HOST = '0.0.0.0';
 
 // App
 const app = express();
-app.use('/', express.static(__dirname + '/wwww'));
+app.use('/', express.static(__dirname));
 
 app.listen(PORT, HOST);
 console.log(`Running on http://${HOST}:${PORT}`);
